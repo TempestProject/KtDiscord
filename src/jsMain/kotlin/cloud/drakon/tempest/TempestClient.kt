ccfe2b5c27670e074acc322b725f85707abd87fc
Adam Chance <6444703+drakon64@users.noreply.github.com>
2022-11-08 22:33:50 +0000

    Use Ktor Content-Type

--- src/jsMain/kotlin/cloud/drakon/tempest/TempestClient.kt
+++ src/jsMain/kotlin/cloud/drakon/tempest/TempestClient.kt
@@ -3,11 +3,12 @@ package cloud.drakon.tempest
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.js.Js
 import io.ktor.client.plugins.defaultRequest
-import io.ktor.client.request.header
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
 
 actual val KtorClient: HttpClient = HttpClient(Js) {
     defaultRequest {
-        header("Content-Type", "application/json")
-        url("https://discord.com/api/v10")
+        contentType(ContentType.Application.Json)
+        url("https://discord.com/api/v10/")
     }
 }
--- src/jvmMain/kotlin/cloud/drakon/tempest/TempestClient.kt
+++ src/jvmMain/kotlin/cloud/drakon/tempest/TempestClient.kt
@@ -3,11 +3,12 @@ package cloud.drakon.tempest
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.java.Java
 import io.ktor.client.plugins.defaultRequest
-import io.ktor.client.request.header
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
 
 actual val KtorClient: HttpClient = HttpClient(Java) {
     defaultRequest {
-        header("Content-Type", "application/json")
-        url("https://discord.com/api/v10")
+        contentType(ContentType.Application.Json)
+        url("https://discord.com/api/v10/")
     }
 }
