6f6f5f1e154113fa14172073109ccef48a92b75d
Adam Chance <6444703+drakon64@users.noreply.github.com>
2022-11-27 21:06:18 +0000

    Implement all Interactions functions

---
+++ src/commonMain/kotlin/webbook/ExecuteWebhook.kt
@@ -0,0 +1,18 @@
+package cloud.drakon.tempest.webbook
+
+import cloud.drakon.tempest.channel.Attachment
+import cloud.drakon.tempest.channel.allowedmentions.AllowedMentions
+import cloud.drakon.tempest.channel.embed.Embed
+import cloud.drakon.tempest.components.Component
+
+class ExecuteWebhook(
+    val content: String? = null,
+    val tts: Boolean? = null,
+    val embeds: Array<Embed>? = null,
+    val allowed_mentions: AllowedMentions? = null,
+    val components: Array<Component>? = null,
+    val payload_json: String? = null,
+    val attachments: Array<Attachment>? = null,
+    val flags: Byte? = null,
+    val thread_name: String? = null,
+)
--- src/jvmMain/kotlin/TempestClient.kt
+++ src/jvmMain/kotlin/TempestClient.kt
@@ -3,6 +3,7 @@ package cloud.drakon.tempest
 import cloud.drakon.tempest.channel.message.Message
 import cloud.drakon.tempest.interaction.response.InteractionResponse
 import cloud.drakon.tempest.webbook.EditWebhookMessage
+import cloud.drakon.tempest.webbook.ExecuteWebhook
 import com.goterl.lazysodium.LazySodiumJava
 import com.goterl.lazysodium.SodiumJava
 import com.goterl.lazysodium.utils.Key
@@ -13,6 +14,7 @@ import io.ktor.client.engine.java.Java
 import io.ktor.client.plugins.DefaultRequest
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.request.delete
 import io.ktor.client.request.get
 import io.ktor.client.request.patch
 import io.ktor.client.request.post
@@ -50,7 +52,7 @@ actual class TempestClient actual constructor(
     suspend fun createInteractionResponse(
         interactionResponse: InteractionResponse,
         interactionId: String,
-        interactionToken: String
+        interactionToken: String,
     ): HttpResponse {
         return ktorClient.post("interactions/$interactionId/$interactionToken/callback") {
             contentType(ContentType.Application.Json)
@@ -59,18 +61,58 @@ actual class TempestClient actual constructor(
     }
 
     suspend fun getOriginalInteractionResponse(
-        interactionToken: String
+        interactionToken: String,
     ): Message {
         return ktorClient.get("webhooks/$applicationId/$interactionToken/messages/@original")
             .body()
     }
 
     suspend fun editOriginalInteractionResponse(
-        editWebhookMessage: EditWebhookMessage, interactionToken: String
+        editWebhookMessage: EditWebhookMessage, interactionToken: String,
     ): Message {
         return ktorClient.patch("webhooks/$applicationId/$interactionToken/messages/@original") {
             contentType(ContentType.Application.Json)
             setBody(editWebhookMessage)
         }.body()
+    }
+
+    suspend fun deleteOriginalInteractionResponse(interactionToken: String): HttpResponse {
+        return ktorClient.delete("webhooks/$applicationId/$interactionToken/messages/@original")
+    }
+
+    suspend fun createFollowupMessage(
+        executeWebhook: ExecuteWebhook,
+        interactionToken: String,
+    ): Message {
+        return ktorClient.post("webhooks/$applicationId/$interactionToken") {
+            contentType(ContentType.Application.Json)
+            setBody(executeWebhook)
+        }.body()
+    }
+
+    suspend fun getFollowupMessage(
+        messageId: String,
+        interactionToken: String,
+    ): Message {
+        return ktorClient.get("webhooks/$applicationId/$interactionToken/messages/$messageId")
+            .body()
+    }
+
+    suspend fun editFollowupMessage(
+        editWebhookMessage: EditWebhookMessage,
+        interactionToken: String,
+        messageId: String,
+    ): Message {
+        return ktorClient.patch("webhooks/$applicationId/$interactionToken/messages/$messageId") {
+            contentType(ContentType.Application.Json)
+            setBody(editWebhookMessage)
+        }.body()
+    }
+
+    suspend fun deleteFollowupMessage(
+        interactionToken: String,
+        messageId: String,
+    ): HttpResponse {
+        return ktorClient.delete("webhooks/$applicationId/$interactionToken/messages/$messageId")
     }
 }
