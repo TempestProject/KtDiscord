f91239182e691a10bd3f760028279a883fb29f5c
Adam Chance <6444703+drakon64@users.noreply.github.com>
2022-11-27 13:06:28 +0000

    Bump `plugin.serialization` from 1.7.20 to 1.7.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("multiplatform") version "1.7.21"
-    kotlin("plugin.serialization") version "1.7.20"
+    kotlin("plugin.serialization") version "1.7.21"
     id("maven-publish")
     id("org.jetbrains.dokka") version "1.7.20"
 }
