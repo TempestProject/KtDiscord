738b23040bde7a988286bc5e5c469d29fd3bba1b
Adam Chance <6444703+drakon64@users.noreply.github.com>
2022-12-30 13:03:47 +0000

    Follow Kotlin coding conventions in the `selectmenu` package

--- src/main/kotlin/components/selectmenu/TextSelectMenu.kt
+++ src/main/kotlin/components/selectmenu/TextSelectMenu.kt
@@ -1,19 +1,21 @@
 package cloud.drakon.discordkt.components.selectmenu
 
+import kotlinx.serialization.SerialName
+
 /**
- * @property custom_id ID for the select menu; max 100 characters
+ * @property customId ID for the select menu; max 100 characters
  * @property options Specified choices in a select menu
  * @property placeholder Placeholder text if nothing is selected; max 150 characters
- * @property min_values Minimum number of items that must be chosen (defaults to 1); min 0, max 25
- * @property max_values Maximum number of items that can be chosen (defaults to 1); max 25
+ * @property minValues Minimum number of items that must be chosen (defaults to 1); min 0, max 25
+ * @property maxValues Maximum number of items that can be chosen (defaults to 1); max 25
  * @property disabled Whether select menu is disabled (defaults to `false`)
  */
 class TextSelectMenu(
-    val custom_id: String,
+    @SerialName("custom_id") val customId: String,
     val options: Array<SelectOption>? = null,
     val placeholder: String? = null,
-    val min_values: Byte? = null,
-    val max_values: Byte? = null,
+    @SerialName("min_values") val minValues: Byte? = null,
+    @SerialName("max_values") val maxValues: Byte? = null,
     val disabled: Boolean? = null,
 ): SelectMenu {
     val type: Byte = 3
--- src/main/kotlin/components/selectmenu/UserSelectMenu.kt
+++ src/main/kotlin/components/selectmenu/UserSelectMenu.kt
@@ -1,17 +1,19 @@
 package cloud.drakon.discordkt.components.selectmenu
 
+import kotlinx.serialization.SerialName
+
 /**
- * @property custom_id ID for the select menu; max 100 characters
+ * @property customId ID for the select menu; max 100 characters
  * @property placeholder Placeholder text if nothing is selected; max 150 characters
- * @property min_values Minimum number of items that must be chosen (defaults to 1); min 0, max 25
- * @property max_values Maximum number of items that can be chosen (defaults to 1); max 25
+ * @property minValues Minimum number of items that must be chosen (defaults to 1); min 0, max 25
+ * @property maxValues Maximum number of items that can be chosen (defaults to 1); max 25
  * @property disabled Whether select menu is disabled (defaults to `false`)
  */
 class UserSelectMenu(
-    val custom_id: String,
+    @SerialName("custom_id") val customId: String,
     val placeholder: String? = null,
-    val min_values: Byte? = null,
-    val max_values: Byte? = null,
+    @SerialName("min_values") val minValues: Byte? = null,
+    @SerialName("max_values") val maxValues: Byte? = null,
     val disabled: Boolean? = null,
 ): SelectMenu {
     val type: Byte = 5
