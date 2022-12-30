338e3543780d3e58ce2f27cfca531f4a47bbba5d
Adam Chance <6444703+drakon64@users.noreply.github.com>
2022-12-30 13:10:50 +0000

    Follow Kotlin coding conventions in the `MessageCallbackData` class

--- src/main/kotlin/interaction/response/interactioncallbackdata/MessageCallbackData.kt
+++ src/main/kotlin/interaction/response/interactioncallbackdata/MessageCallbackData.kt
@@ -4,13 +4,14 @@ import cloud.drakon.discordkt.channel.Attachment
 import cloud.drakon.discordkt.channel.allowedmentions.AllowedMentions
 import cloud.drakon.discordkt.channel.embed.Embed
 import cloud.drakon.discordkt.components.Component
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 /**
  * @property tts is the response TTS
  * @property content message content
  * @property embeds supports up to 10 embeds
- * @property allowed_mentions allowed mentions object
+ * @property allowedMentions allowed mentions object
  * @property flags message flags combined as a bitfield (only `SUPPRESS_EMBEDS` and `EPHEMERAL` can be set)
  * @property components message components
  * @property attachments attachment objects with filename and description
@@ -19,7 +20,7 @@ import kotlinx.serialization.Serializable
     val tts: Boolean? = null,
     val content: String? = null,
     val embeds: Array<Embed>? = null,
-    val allowed_mentions: AllowedMentions? = null,
+    @SerialName("allowed_mentions") val allowedMentions: AllowedMentions? = null,
     val flags: Short? = null,
     val components: Array<Component>? = null,
     val attachments: Array<Attachment>? = null,
