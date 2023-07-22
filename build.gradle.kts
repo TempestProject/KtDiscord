7557192be31586b1db9386d62ee9cbc60059a203
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-07-22 19:47:18 +0100

    Re-enable publishing to GitHub Packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -130,6 +130,17 @@ publishing {
             }
         }
     }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/TempestProject/KtDiscord")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
 
 signing {
